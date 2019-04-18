7211fca677b61195edaf4cc4cef533e9068c08be
BigMom <johnpaulc@163.com>
2019-04-18 20:44:24 +0800

    First version

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 
 
 class Conv_NN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(Conv_NN, self).__init__()
         # input channel, output channel, Filter Size
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5)
         # Filter Size
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -16,10 +17,12 @@ class Conv_NN(nn.Module):
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
+
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
+        print(x.shape)
         x = self.pool(F.relu(self.conv2(x)))
-
+        print(x.shape)
         # Change to 1 dimensional Tensor
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 from torchvision.datasets import MNIST
 from torchvision import transforms
+from model import Conv_NN
 
 transform = transforms.Compose([
     # you can add other transformations in this list
@@ -11,20 +12,25 @@ transform = transforms.Compose([
 trainset = MNIST('MNIST', transform = transform)
 
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size = 10,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size = 1,
                                         shuffle = True, num_workers = 2)
 print(trainset)
 
 print(trainloader)
 write_flag = 0
 
+model = Conv_NN()
+
 for i, train_data in enumerate(trainloader):
     if write_flag == 1:
         break
     print(type(train_data))
-    train_img = train_data[0]
+    train_img = train_data[0].squeeze()
     label = train_data[1]
     print(type(train_img))
     print(train_img.shape)
     print(type(label))
+
+    a = model(train_img)
+    print(a)
     write_flag += 1
