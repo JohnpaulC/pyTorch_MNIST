719711b8924bb9b3a2b017628b571cb5945bf764
BigMom <johnpaulc@163.com>
2019-04-19 08:04:20 +0800

    "First Versino"

--- train.py
+++ train.py
@@ -4,33 +4,25 @@ from torchvision.datasets import MNIST
 from torchvision import transforms
 from model import Conv_NN
 
+# At least one transformation
 transform = transforms.Compose([
-    # you can add other transformations in this list
     transforms.ToTensor()
 ])
 
 trainset = MNIST('MNIST', transform = transform)
-
-
 trainloader = torch.utils.data.DataLoader(trainset, batch_size = 1,
                                         shuffle = True, num_workers = 2)
-print(trainset)
 
-print(trainloader)
-write_flag = 0
-
+Debug_flag = 0
 model = Conv_NN()
 
 for i, train_data in enumerate(trainloader):
-    if write_flag == 1:
+    if Debug_flag == 1:
         break
-    print(type(train_data))
+
     train_img = train_data[0].squeeze()
     label = train_data[1]
-    print(type(train_img))
-    print(train_img.shape)
-    print(type(label))
 
     a = model(train_img)
     print(a)
-    write_flag += 1
+    Debug_flag += 1
