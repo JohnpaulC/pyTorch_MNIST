e40b409f5b9df03013efbd8c0e7cc38f62502269
Johnpaul <Johnpaulc@foxmail.com>
2019-04-28 16:27:45 +0800

    Final Version 0.0

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from torchvision import transforms
 from torchvision.datasets import MNIST
 import matplotlib.pyplot as plt
 
-batch_size = 8
+batch_size = 4
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = ConvNN()
 model.load_state_dict(torch.load('model.pt', map_location=device))
@@ -29,9 +29,11 @@ with torch.no_grad():
         _, pred = model(test_img, apply_softmax=True).max(dim=1)
 
         print(pred)
+        pred = pred.numpy()
         plt.figure()
         for i in range(batch_size):
-            plt.subplot(4, 2, i + 1)
+            plt.subplot(2, 2, i + 1).set_title("This is " + str(pred[i]))
+            plt.axis('off')
             plt.imshow(test_img[i, ...].squeeze().numpy(), 'Greys_r')
         plt.show()
 
--- utils.py
+++ utils.py
@@ -6,23 +6,3 @@ def get_accuracy(y_pred, y_target):
     n_correct = torch.eq(y_pred, y_target).sum().item()
     accuracy = n_correct / len(y_pred) * 100
     return accuracy
-
-
-def imshow(image_show, title=None):
-    # Change C x H x W into H x W x C to show the image(plt)
-    image_show = image_show.numpy().transpose((1, 2, 0))
-    # Re-Normalize the imgage
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-
-    image_show = std * image_show + mean
-    # Limit the value into approperiate interval
-    # RGB image float (0, 1) and integers (0, 255)
-    image_show = np.clip(image_show, 0, 1)
-
-    plt.imshow(image_show)
-
-    # Show the label of each images in the grid
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)
